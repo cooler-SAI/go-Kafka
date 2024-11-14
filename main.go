b681d4dda8d33874d80828a6d0563fc78502ed93
cooler-SAI <cooler19860212@yahoo.com>
2024-11-15 00:34:29 +0300

    added all stuff foe kafka work

--- main.go
+++ main.go
@@ -8,13 +8,53 @@ import (
 	"time"
 )
 
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Info().Msg("Starting Kafka Go application...")
+
+	go startConsumer()
+	go startProducer()
+
+	select {}
+}
+
+func startProducer() {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, config)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to start Kafka producer")
+	}
+	defer func(producer sarama.SyncProducer) {
+		err := producer.Close()
+		if err != nil {
+
+		}
+	}(producer)
+
+	for {
+		msg := &sarama.ProducerMessage{
+			Topic: "first-topic",
+			Value: sarama.StringEncoder("Hello from Kafka producer!"),
+		}
+		partition, offset, err := producer.SendMessage(msg)
+		if err != nil {
+			log.Error().Err(err).Msg("Failed to send message")
+		} else {
+			log.Info().Msgf("Message is stored in topic(%s)/partition(%d)/offset(%d)", "first-topic", partition, offset)
+		}
+		time.Sleep(3 * time.Second)
+	}
+}
+
 func startConsumer() {
 	config := sarama.NewConfig()
 	config.Consumer.Return.Errors = true
 
 	consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, config)
 	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to start consumer")
+		log.Fatal().Err(err).Msg("Failed to start Kafka consumer")
 	}
 	defer func(consumer sarama.Consumer) {
 		err := consumer.Close()
@@ -23,34 +63,29 @@ func startConsumer() {
 		}
 	}(consumer)
 
-	partitionConsumer, err := consumer.ConsumePartition("first-topic",
-		0, sarama.OffsetNewest)
+	partitions, err := consumer.Partitions("first-topic")
 	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to start partition consumer")
-	}
-	defer func(partitionConsumer sarama.PartitionConsumer) {
-		err := partitionConsumer.Close()
-		if err != nil {
-
-		}
-	}(partitionConsumer)
-
-	for msg := range partitionConsumer.Messages() {
-		log.Info().Msgf("Message on topic: %s, partition: %d, offset: %d\n",
-			msg.Topic, msg.Partition, msg.Offset)
+		log.Fatal().Err(err).Msg("Failed to get partitions for topic")
 	}
 
-}
+	for _, partition := range partitions {
+		go func(partition int32) {
+			partitionConsumer, err := consumer.ConsumePartition("first-topic", partition, sarama.OffsetNewest)
+			if err != nil {
+				log.Fatal().Err(err).Msgf("Failed to start partition consumer for partition %d", partition)
+			}
+			defer func(partitionConsumer sarama.PartitionConsumer) {
+				err := partitionConsumer.Close()
+				if err != nil {
 
-func main() {
+				}
+			}(partitionConsumer)
 
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-	log.Info().Msg("Hello Kafka Go!")
-
-	go startConsumer()
-
-	for {
-		time.Sleep(3 * time.Second)
-		log.Info().Msg("Scanning.....")
+			for msg := range partitionConsumer.Messages() {
+				log.Info().Msgf("Received message from partition %d: %s", partition, string(msg.Value))
+			}
+		}(partition)
 	}
+
+	select {}
 }
