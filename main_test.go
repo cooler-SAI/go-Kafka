43ee48721bed3ac575dbd5be12d73ece1356d219
cooler-SAI <cooler19860212@yahoo.com>
2024-11-16 22:59:48 +0300

    added logs to unit tests

--- main_test.go
+++ main_test.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"github.com/IBM/sarama"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
+func logInit() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	log.Info().Msg("Init zerolog...")
+}
+
 func TestStartProducer(t *testing.T) {
+	logInit()
+
+	log.Info().Msg("Starting Test Kafka Producer application...")
 	config := sarama.NewConfig()
 	config.Producer.Return.Successes = true
 
 	config.Producer.Return.Errors = true
 
+	log.Info().Msg("Init topic and start sync...")
 	producer, err := sarama.NewSyncProducer([]string{"127.0.0.1:9092"}, config)
 	assert.NoError(t, err, "Failed to create Kafka Producer")
 	defer func(producer sarama.SyncProducer) {
@@ -30,5 +42,7 @@ func TestStartProducer(t *testing.T) {
 	assert.NoError(t, err, "Failed to send message")
 	assert.GreaterOrEqual(t, partition, int32(0), "Invalid partition")
 	assert.GreaterOrEqual(t, offset, int64(0), "Invalid offset")
+
+	log.Info().Msg("Test Producer Kafka Successfully Initialized")
 
 }
