8bc18dc481291b4ecb7d05a3d077473656a9053b
cooler-SAI <cooler19860212@yahoo.com>
2024-12-09 22:15:59 +0300

    optimized code part 2

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/IBM/sarama"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"go-Kafka/kafka"
@@ -10,52 +9,6 @@ import (
 	"syscall"
 )
 
-func startConsumer() {
-	config := sarama.NewConfig()
-	config.Consumer.Return.Errors = true
-
-	consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, config)
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to start Kafka consumer")
-	}
-	defer func(consumer sarama.Consumer) {
-		err := consumer.Close()
-		if err != nil {
-
-		}
-	}(consumer)
-
-	partitions, err := consumer.Partitions("first-topic")
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to get partitions for topic")
-	}
-
-	for _, partition := range partitions {
-		go func(partition int32) {
-			partitionConsumer, err := consumer.ConsumePartition("first-topic",
-				partition, sarama.OffsetNewest)
-			if err != nil {
-				log.Fatal().Err(err).Msgf("Failed to start partition consumer "+
-					"for partition %d", partition)
-			}
-			defer func(partitionConsumer sarama.PartitionConsumer) {
-				err := partitionConsumer.Close()
-				if err != nil {
-
-				}
-			}(partitionConsumer)
-
-			for msg := range partitionConsumer.Messages() {
-				log.Log().Msg("Consumer:")
-				log.Info().Msgf("Received message from partition %d: %s",
-					partition, string(msg.Value))
-			}
-		}(partition)
-	}
-
-	select {}
-}
-
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	log.Info().Msg("Starting Kafka Go application...")
@@ -63,8 +16,8 @@ func main() {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
-	go startConsumer()
 	go kafka.StartProducer()
+	go kafka.StartConsumer()
 
 	<-stop
 
